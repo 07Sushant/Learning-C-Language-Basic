71ad01f02c199eb5b99213acd0746f34c91bf29c
Sushant <sushantkumarkohima@gmail.com>
2023-02-06 00:18:14 +0530

    first day

---
+++ add.c
@@ -0,0 +1,13 @@
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter number the first number: \n");
+    scanf("%d", &a);
+
+    printf("Enter number Second number: \n");
+    scanf("%d", &b);
+
+    printf("The sum is:  %d \n", a+b);
+
+    return 0;
+}
